d19cb2b5d19a9a0b87dc341c01b223e3e1b50dbe
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-06-30 09:28:05 +0200

    Added Sepolia test network configuration and Alchemy API key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,19 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { vars, HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers"
 
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
