ecd0abbaa11fba330cc9d23a08edea451de97a21
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-06-30 12:02:42 +0200

    Code cleanup

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,16 +2,15 @@ import { ethers, network, run } from "hardhat"
 
 async function main() {
   // We get the contract to deploy
-  const Box = await ethers.getContractFactory('Box');
+  const BoxFactory = await ethers.getContractFactory('Box');
   console.log('Deploying Box...');
-  const box = await Box.deploy();
+  const box = await BoxFactory.deploy();
   await box.waitForDeployment();
   const address = await box.getAddress();
   console.log('Box deployed to: ', address);
   console.log(network.config);
-  console.log(network.config.chainId);
   if (network.config.chainId === 11155111) {
-    await verify(address, []);
+    await verify(box.target, []);
   }
 }
 
@@ -29,7 +28,6 @@ async function verify(contractAddress, args) {
       console.log(e);
     }
   }
-  await run("verify");
 }
 
 main()
