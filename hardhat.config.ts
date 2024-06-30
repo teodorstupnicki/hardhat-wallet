96e595a0f9ca48e16d007baed9c04758adeebb99
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-06-30 11:34:37 +0200

    Updated Sepolia chainId property

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,15 +5,20 @@ import "@nomicfoundation/hardhat-chai-matchers"
 
 const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
 const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
+      chainId: 11155111,
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY]
     }
-  }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
